e3e2097e55ad5825ef27706fe3ecb2a414d87c2f
JackMarx <seriousfools@gmail.com>
2018-09-02 10:42:52 -0500

    model methods

--- runner.rb
+++ runner.rb
@@ -2,8 +2,8 @@ require 'http'
 
 # index
 response = HTTP.get("http://localhost:3000/api/recipes")
-p response.parse
+p response.parse[0]["ingredients"]
 
 # show
-response = HTTP.get("http://localhost:3000/api/recipes/1")
-p response.parse
+# response = HTTP.get("http://localhost:3000/api/recipes/1")
+# p response.parse
