ec431cebaccc873dee7f410a8ad5fb9341f739b8
JackMarx <seriousfools@gmail.com>
2018-10-11 18:19:45 -0500

    clone templates

---
+++ public/index.js
@@ -0,0 +1,9 @@
+var recipeTemplate = document.querySelector('#recipe-card');
+var recipeContainer = document.querySelector('.row');
+
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
