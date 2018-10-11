2f38119b3693334af515571f724d616d9ffb8bad
JackMarx <seriousfools@gmail.com>
2018-10-11 18:51:20 -0500

    dynamic clones

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
 var recipeTemplate = document.querySelector('#recipe-card');
 var recipeContainer = document.querySelector('.row');
 
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+
+axios.get("http://localhost:3000/api/recipes").then(function(response) {
+  var recipes = response.data;
+  console.log(recipes);
+
+  recipes.forEach(function(recipe) {
+    var recipeClone = recipeTemplate.content.cloneNode(true);
+
+    recipeClone.querySelector('.recipe-title').innerText = recipe.title;
+    recipeClone.querySelector('.ingredients').innerText = recipe.ingredients;
+    recipeClone.querySelector('.prep-time').innerText = recipe["formatted"]["prep_time"];
+
+    recipeContainer.appendChild(recipeClone);
+  });
+});
